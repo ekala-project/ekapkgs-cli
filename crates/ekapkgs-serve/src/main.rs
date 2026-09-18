ebb6c24557a36178311a412438e189cae8dc6d16
Jonathan Ringer <jonringer117@gmail.com>
2026-09-18 16:48:52 -0700

    Add GET /version endpoint

--- crates/ekapkgs-serve/src/main.rs
+++ crates/ekapkgs-serve/src/main.rs
@@ -223,6 +223,7 @@ fn build_http_router(state: Arc<AppState>) -> Router {
 
     Router::new()
         .route("/health", get(api::compat::health))
+        .route("/version", get(api::compat::version))
         .route("/nix-cache-info", get(api::compat::nix_cache_info))
         .route(
             "/{hash_narinfo}",
